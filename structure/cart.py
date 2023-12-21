33d82e4a9d54bbf49550192e86fd893dec0076da
yeyeye618 <t110590009@ntut.org.tw>
2023-12-22 00:09:21 +0800

    feat:新增remove(cart, actual card, comment) update(comment)

--- structure/cart.py
+++ structure/cart.py
@@ -1,6 +1,6 @@
 import mysql as sql
 # 查購物車
-def GetCart(data:dict):
+def GetCardInCart(data:dict):
     cmd = f'''select sc.ID,Price,sc.Status,sc.Quantity,sc.ACCard_ID,sc.Store_ID
     from StoreCard sc
     inner join Card_to_Cart_TableID cctable ON cctable.Card_ID = sc.ID
@@ -10,8 +10,15 @@ def GetCart(data:dict):
     return sql.command(cmd)
 
 # 加入Store card到 shopping cart
-def AddCardToCart(data:dict):
+def AddCardtoCart(data:dict):
     id = sql.getMaxId("Card_to_Cart_TableID") + 1
-    card_to_cart_arg = [id, data['Cart_ID'],data['Card_id']]
+    card_to_cart_arg = [id, data['Cart_id'],data['Card_id']]
     sql.command(sql.insert("Card_to_Cart_TableID",card_to_cart_arg))
     return "added"
+
+# remove card from shopping cart
+def removeCardFromCart(data:dict):
+    if sql.countTable(f"Card_to_Cart_TableID where Card_ID = {data['Card_id']}") == 0:
+        return "Card_ID not exists"
+    sql.command(f"Delete From Card_to_Cart_TableID where Card_ID = {data['Card_id']} and Cart_ID = {data['Cart_id']}")
+    return "removed"
--- structure/comment.py
+++ structure/comment.py
@@ -5,3 +5,19 @@ def AddComment(data:dict):
     Comment_arg = [id,data['score'],data['context'],data['store_id'],data['user_id']]
     result = sql.command(sql.insert("Comment",Comment_arg))
     return "added" if not result else "add failed"
+
+# update comment
+def updateComment(data:dict):
+    if sql.countTable(f"Comment where ID = {data['Comment_id']}") == 0:
+        return "Comment not found"
+    condition = [f"Score = {data['score']}"] if data.get('score') != None else []
+    condition += [f"Context = '{data['context']}'"] if data.get('context') != None else []
+    sql.command(f"update Comment set {','.join(condition)} where ID = {data['Comment_id']}")
+    return "Comment updated"
+
+# delete comment
+def removeComment(data:dict):
+    if sql.countTable(f"Comment where ID = {data['Comment_id']}") == 0:
+        return "Comment not found"
+    sql.command(f"DELETE FROM Comment where ID = {data['Comment_id']}")
+    return "Comment deleted"
