9d6af98d71e48ed619eadfd392261177a1b3abeb
Madfater <ericleeddd@gmail.com>
2023-12-27 20:12:57 +0000

    feat: 修改初始路由

--- api.py
+++ api.py
@@ -2,8 +2,25 @@ from flask import Flask, jsonify, request
 from structure import card,cart,order,store,user,comment
 from flask_cors import CORS
 
+class PrefixMiddleware(object):
+
+    def __init__(self, app, prefix=''):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response('404', [('Content-Type', 'text/plain')])
+            return ["This url does not belong to the app.".encode()]
+
 app = Flask(__name__)
 CORS(app)
+app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/api')
 
 # User
 @app.route('/user/<path:method>', methods = ['POST'])
